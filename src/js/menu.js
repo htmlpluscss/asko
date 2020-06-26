696ddec1b86704f45fb73e0f44dd89f865409374
htmlpluscss <79198889134@ya.ru>
2020-06-26 19:19:45 +0300

    360

--- src/js/menu.js
+++ src/js/menu.js
@@ -6,7 +6,10 @@
 
 	}
 
-	var level1 = nav.querySelectorAll('.nav__item');
+	var level1 = nav.querySelectorAll('.nav__item'),
+		level2Parent = nav.querySelectorAll('.nav__level2-item--parent'),
+		level2Back = nav.querySelectorAll('.nav__level2-btn-back'),
+		level3Back = nav.querySelectorAll('.nav__level3-btn-back');
 
 	// открыть|закрыть меню
 
@@ -19,6 +22,7 @@
 				Array.prototype.forEach.call(level1, function(el){
 
 					el.classList.remove('is-hover');
+					nav.classList.remove('is-level2');
 
 				});
 
@@ -41,16 +45,44 @@
 			el.addEventListener('mouseleave', function () {
 
 				el.classList.remove('is-hover');
+				nav.classList.remove('is-level2');
+
+			});
+
+			el.addEventListener('mouseenter', function () {
+
+				el.classList.remove('is-hover');
+				nav.classList.remove('is-level2');
+
+				Array.prototype.forEach.call(level1, function(elem){
+
+					elem.classList.remove('is-hover');
+
+				});
 
 			});
 
 		}
 		else {
 
+			el.addEventListener('mouseenter', function () {
+
+				nav.classList.add('is-level2');
+
+				Array.prototype.forEach.call(level1, function(elem){
+
+					elem.classList.toggle('is-hover', elem === el);
+
+				});
+
+			});
+
 			el.querySelector('.nav__head').addEventListener('touchstart', function (e) {
 
 				e.preventDefault();
 				e.stopPropagation();
+
+				nav.classList.add('is-level2');
 
 				Array.prototype.forEach.call(level1, function(elem){
 
@@ -62,11 +94,53 @@
 
 		}
 
-		el.addEventListener('mouseenter', function () {
+	});
 
-			Array.prototype.forEach.call(level1, function(elem){
+	// клик второй уровень
+
+	Array.prototype.forEach.call(level2Parent, function(el){
+
+		el.addEventListener('touchstart', function () {
+
+			nav.classList.add('is-level3');
+
+			Array.prototype.forEach.call(level2Parent, function(elem){
 
 				elem.classList.toggle('is-hover', elem === el);
+
+			});
+
+		});
+
+	});
+
+	// клик по кнопке назад
+
+	Array.prototype.forEach.call(level2Back, function(el){
+
+		el.addEventListener('click', function () {
+
+			nav.classList.remove('is-level2');
+
+			Array.prototype.forEach.call(level1, function(el){
+
+				el.classList.remove('is-hover');
+
+			});
+
+		});
+
+	});
+
+	Array.prototype.forEach.call(level3Back, function(el){
+
+		el.addEventListener('click', function () {
+
+			nav.classList.remove('is-level3');
+
+			Array.prototype.forEach.call(level2Parent, function(elem){
+
+				elem.classList.remove('is-hover');
 
 			});
 
