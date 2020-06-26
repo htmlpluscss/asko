e530d3352d70878c97233af965b9ade2fdf2a534
htmlpluscss <79198889134@ya.ru>
2020-06-26 19:19:45 +0300

    v2

--- src/js/menu.js
+++ src/js/menu.js
@@ -6,7 +6,10 @@
 
 	}
 
-	var level1 = nav.querySelectorAll('.nav__item');
+	var level1 = nav.querySelectorAll('.nav__item'),
+		level2Parent = nav.querySelectorAll('.nav__level2-item--parent'),
+		level2Back = nav.querySelectorAll('.nav__level2-btn-back'),
+		level3Back = nav.querySelectorAll('.nav__level3-btn-back');
 
 	// открыть|закрыть меню
 
@@ -16,11 +19,32 @@
 
 			if(ASKO.body.classList.contains('menu-open')){
 
+				nav.classList.remove('is-level2');
+				nav.classList.remove('is-level3');
+
 				Array.prototype.forEach.call(level1, function(el){
 
 					el.classList.remove('is-hover');
 
 				});
+
+				Array.prototype.forEach.call(level2Parent, function(el){
+
+					el.classList.remove('is-hover');
+
+				});
+
+				setTimeout(function(){
+
+					window.scrollTo(0,ASKO.windowScrollOld);
+
+				});
+
+			}
+			else {
+
+				ASKO.windowScrollOld = window.pageYOffset;
+				window.scrollTo(0,0);
 
 			}
 
@@ -41,16 +65,47 @@
 			el.addEventListener('mouseleave', function () {
 
 				el.classList.remove('is-hover');
+				nav.classList.remove('is-level2');
+
+			});
+
+			el.addEventListener('mouseenter', function () {
+
+				el.classList.remove('is-hover');
+				nav.classList.remove('is-level2');
+
+				Array.prototype.forEach.call(level1, function(elem){
+
+					elem.classList.remove('is-hover');
+
+				});
 
 			});
 
 		}
 		else {
 
-			el.querySelector('.nav__head').addEventListener('touchstart', function (e) {
+			el.addEventListener('mouseenter', function () {
+
+				nav.classList.add('is-level2');
+
+				Array.prototype.forEach.call(level1, function(elem){
+
+					elem.classList.toggle('is-hover', elem === el);
+
+				});
+
+			});
+
+//			el.querySelector('.nav__head').addEventListener('touchstart', function (e) {
+			el.querySelector('.nav__head').addEventListener('click', function (e) {
 
 				e.preventDefault();
 				e.stopPropagation();
+
+				nav.style.minHeight = el.querySelector('.nav__level2').scrollHeight + "px";
+
+				nav.classList.add('is-level2');
 
 				Array.prototype.forEach.call(level1, function(elem){
 
@@ -62,9 +117,45 @@
 
 		}
 
-		el.addEventListener('mouseenter', function () {
+	});
 
-			Array.prototype.forEach.call(level1, function(elem){
+	// ховер/клик второй уровень
+
+	Array.prototype.forEach.call(level2Parent, function(el){
+
+		el.parentNode.addEventListener('mouseenter', function () {
+
+			nav.classList.add('is-level3');
+
+			Array.prototype.forEach.call(level2Parent, function(elem){
+
+				elem.classList.toggle('is-hover', elem === el);
+
+			});
+
+		});
+
+		el.parentNode.addEventListener('mouseleave', function () {
+
+			nav.classList.remove('is-level3');
+
+			Array.prototype.forEach.call(level2Parent, function(elem){
+
+				elem.classList.remove('is-hover');
+
+			});
+
+		});
+
+//		el.querySelector('.nav__level2-link').addEventListener('touchstart', function (e) {
+		el.querySelector('.nav__level2-link').addEventListener('click', function (e) {
+
+			e.preventDefault();
+			e.stopPropagation();
+
+			nav.classList.add('is-level3');
+
+			Array.prototype.forEach.call(level2Parent, function(elem){
 
 				elem.classList.toggle('is-hover', elem === el);
 
@@ -73,5 +164,44 @@
 		});
 
 	});
+
+	// клик по кнопке назад
+
+	Array.prototype.forEach.call(level2Back, function(el){
+
+		el.addEventListener('click', function () {
+
+			nav.style.minHeight = nav.querySelector('.nav__inner').clientHeight + "px";
+
+			nav.classList.remove('is-level2');
+
+			Array.prototype.forEach.call(level1, function(el){
+
+				el.classList.remove('is-hover');
+
+			});
+
+		});
+
+	});
+
+	Array.prototype.forEach.call(level3Back, function(el){
+
+		el.addEventListener('click', function () {
+
+			nav.classList.remove('is-level3');
+
+			Array.prototype.forEach.call(level2Parent, function(elem){
+
+				elem.classList.remove('is-hover');
+
+			});
+
+		});
+
+	});
+
+	// слушаем анимацию блока (смещение по горизонтале)
+//	ASKO.cssAnimation('transition')
 
 })(document.querySelector('.nav'));
